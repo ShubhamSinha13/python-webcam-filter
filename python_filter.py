47fd09fe895d1e3af54c98f221f3177ab8fc11c9
ShubhamSinha13 <shubham.sinha2025a@vitstudent.ac.in>
2026-01-11 21:20:48 +0530

    fixed some errors

--- python_filter.py
+++ python_filter.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 #loading the model and importing then image
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-sunglasses = cv2.imread(r"C:\Users\SHUBHAM\OneDrive\Desktop\New folder\sunglasses.png", cv2.IMREAD_UNCHANGED)
+mask = cv2.imread(r"C:\Users\SHUBHAM\OneDrive\Desktop\New folder\mask.png", cv2.IMREAD_UNCHANGED)
 
 #video capturing and logic 
 cap = cv2.VideoCapture(0)
@@ -15,23 +15,20 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
-        # Resize sunglasses to fit the face width
-        sunglass_width = int(w)
-        sunglass_height = int(sunglasses.shape[0] * (sunglass_width / sunglasses.shape[1]))
-        resized_sunglasses = cv2.resize(sunglasses, (sunglass_width, sunglass_height))
+        mask_width = int(w * 1.45)
+        mask_height = int(mask.shape[0] * (mask_width / mask.shape[1]))
+        resized_mask = cv2.resize(mask, (mask_width, mask_height))
 
-        # Calculate position for sunglasses
-        y1 = y + int(h*0.001) 
-        y2 = y1 + sunglass_height
-        x1 = x
-        x2 = x + sunglass_width
+        y1 = y + int(h * 0.3) 
+        y2 = y1 + mask_height
+        x1 = x - int((mask_width - w) / 2)
+        x2 = x1 + mask_width
 
-
-        alpha = resized_sunglasses[:, :, 3] / 255.0 
+        alpha = resized_mask[:, :, 3] / 255.0 
         for c in range(3):
-            frame[y1:y2, x1:x2, c] = (alpha * resized_sunglasses[:, :, c] +
+            frame[y1:y2, x1:x2, c] = (alpha * resized_mask[:, :, c] +
                                       (1 - alpha) * frame[y1:y2, x1:x2, c])
-    cv2.imshow('Sunglasses Filter', frame)
+    cv2.imshow('Mask Filter', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
